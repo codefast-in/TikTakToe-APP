65bef8ce84af68f0b94fed8d849d26c6ce3e66e8
code-fast <sachinspindofficial@gmail.com>
2023-12-18 13:05:01 +0530

    last push

--- src/App.tsx
+++ src/App.tsx
@@ -1,125 +1,428 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  FlatList,
   Pressable,
   SafeAreaView,
- 
+  ScrollView,
+  StatusBar,
   StyleSheet,
   Text,
-  TouchableOpacity,
- 
   View,
 } from 'react-native';
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+import Snackbar from 'react-native-snackbar';
+import Icons from './Components/Icons';
 
-function App(): React.JSX.Element {
+
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
+
+
+function App(): JSX.Element {
+  const [isCross, setIsCross] = useState<boolean>(false);
+  const [gameWinner, setGameWinner] = useState<string>('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+
+  const reloadGame = () => {
+    ReactNativeHapticFeedback.trigger("impactHeavy", options);
+    setIsCross(false);
+    setGameWinner('');
+    setGameState(new Array(9).fill('empty', 0, 9));
+  };
+
+  const checkIsWinner = () => {
+    //  checking  winner of the game
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== 'empty'
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[3] !== 'empty' &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} won the game! 🥳`);
+    } else if (
+      gameState[6] !== 'empty' &&
+      gameState[6] === gameState[7] &&
+      gameState[7] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[1] !== 'empty' &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game... ⌛️');
+    }
+  };
+  const onChangeItem = (itemNumber: number) => {
+    ReactNativeHapticFeedback.trigger("impactHeavy", options);
+    if (gameWinner) {
+      return Snackbar.show({
+        text: gameWinner,
+        backgroundColor: '#000000',
+        textColor: '#FFFFFF',
+      });
+    }
+
+    if (gameState[itemNumber] === 'empty') {
+      gameState[itemNumber] = isCross ? 'cross' : 'circle';
+      setIsCross(!isCross);
+    } else {
+      return Snackbar.show({
+        text: 'Position is already filled',
+        backgroundColor: 'red',
+        textColor: '#FFF',
+      });
+    }
+    
+    checkIsWinner();
+  };
+
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <View style={styles.playerNameBox}>
-          <Text style={styles.playerName}>Player O's Turn</Text>
+      <StatusBar />
+      {gameWinner ? (
+        <View style={[styles.playerInfo, styles.winnerInfo]}>
+          <Text style={styles.winnerTxt}>{gameWinner}</Text>
         </View>
-        <View style={styles.playground}>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>1</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>2</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>3</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>4</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>5</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>6</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>7</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>8</Text>
-          </Pressable>
-          <Pressable style={styles.box}>
-            <Text style={styles.playText}>9</Text>
-          </Pressable>
+      ) : (
+        <View
+          style={[
+            styles.playerInfo,
+            isCross ? styles.playerX : styles.playerO,
+          ]}>
+          <Text style={styles.gameTurnTxt}>
+            Player {isCross ? 'X' : 'O'}'s Turn
+          </Text>
         </View>
-        <View>
-          <TouchableOpacity style={styles.resetBtn} > 
-            <Text style={styles.resetBtnText}>Reload Game</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      )}
+      {/* Game Grid */}
+      <FlatList
+        numColumns={3}
+        data={gameState}
+        style={styles.grid}
+        renderItem={({item, index}) => (
+          <Pressable
+            key={index}
+            style={styles.card}
+            onPress={() => onChangeItem(index)}>
+            <Icons name={item} />
+          </Pressable>
+        )}
+      />
+      {/* game action */}
+      <Pressable style={styles.gameBtn} onPress={reloadGame}>
+        <Text style={styles.gameBtnText}>
+          {gameWinner ? 'Start new game' : 'ReLoad the game'}
+        </Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  playerInfo: {
+    height: 56,
+
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'stretch',
-    marginHorizontal: 10,
-    marginVertical: 20,
-    width:undefined,
-    height:undefined,
-  },
-  playerNameBox: {
-    backgroundColor: '#0A79DF',
     alignItems: 'center',
-    padding: 10,
-    borderRadius: 8,
-    borderWidth: 0.5,
-    borderColor: '#FFFFFF',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
   },
-  playerName: {
+  gameTurnTxt: {
+    fontSize: 20,
     color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 120,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#EAF0F1',
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+   
+    alignItems: 'center',
+
+    padding: 15,
+    borderRadius: 8,
+    marginHorizontal: 12,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
     fontSize: 18,
+    color: '#FFFFFF',
     fontWeight: '500',
   },
-  playground: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems:'stretch',
-    flexDirection:'row',
-    flexWrap:"wrap",
-    marginVertical:20
-  },
-  box:{
-    height:103,
-    width:103,
-    backgroundColor:'#EAF0F1',
-    alignItems:"center",
-    justifyContent:'center',
-    borderRadius:5,
-    margin:5
-    
-  },
-  playText:{
-    color:'#2B2B52'
-  },
-  resetBtn:{
-    backgroundColor: '#E44236',
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 8,
-    borderWidth: 0.5,
-    borderColor: '#FFFFFF',
-  },
-  resetBtnText:{
-    color: '#FFFFFF',
-    fontSize: 18,
-    fontWeight: '500',
-  }
 });
 
 export default App;
+
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
+
+// import React, {useState} from 'react';
+
+// import {
+//   FlatList,
+//   Pressable,
+//   SafeAreaView,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   View,
+// } from 'react-native';
+// import Icons from './Components/Icons';
+// import Snackbar from 'react-native-snackbar';
+
+// function App(): React.JSX.Element {
+//   const [gameWinner, setGameWinner] = useState<string>('empty');
+//   const [isCircle, setIsCircle] = useState<boolean>(false);
+//   const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+//   // const [gameWinned, setgameWinned] = useState<boolean>(false)
+//   const resetGame = () => {
+//     setGameWinner('empty');
+//     setIsCircle(false);
+//     setGameState(new Array(9).fill('empty', 0, 9));
+//   };
+
+//   const winner = () => {
+//     if (
+//       gameState[0] === gameState[1] &&
+//       gameState[1] === gameState[2] &&
+//       gameState[0] !== 'empty'
+//     ) {
+//       setGameWinner(`${gameWinner[0]} Won The Game`);
+//     } else if (
+//       gameState[0] !== 'empty' &&
+//       gameState[0] === gameState[3] &&
+//       gameState[3] === gameState[6]
+//     ) {
+//       setGameWinner(`${gameWinner[0]} Won The Game`);
+//     } else if (
+//       gameState[0] !== 'empty' &&
+//       gameState[0] === gameState[4] &&
+//       gameState[4] === gameState[8]
+//     ) {
+//       setGameWinner(`${gameWinner[0]} Won The Game`);
+//     } else if (
+//       gameState[1] !== 'empty' &&
+//       gameState[1] === gameState[4] &&
+//       gameState[4] === gameState[7]
+//     ) {
+//       setGameWinner(`${gameWinner[1]} Won The Game`);
+//     } else if (
+//       gameState[2] !== 'empty' &&
+//       gameState[2] === gameState[4] &&
+//       gameState[4] === gameState[6]
+//     ) {
+//       setGameWinner(`${gameWinner[2]} Won The Game`);
+//     } else if (
+//       gameState[2] !== 'empty' &&
+//       gameState[2] === gameState[5] &&
+//       gameState[5] === gameState[8]
+//     ) {
+//       setGameWinner(`${gameWinner[2]} Won The Game`);
+//     } else if (
+//       gameState[3] !== 'empty' &&
+//       gameState[3] === gameState[4] &&
+//       gameState[4] === gameState[5]
+//     ) {
+//       setGameWinner(`${gameWinner[3]} Won The Game`);
+//     } else if (
+//       gameState[6] !== 'empty' &&
+//       gameState[6] === gameState[7] &&
+//       gameState[7] === gameState[8]
+//     ) {
+//       setGameWinner(`${gameWinner[6]} Won The Game`);
+//     } else if (!gameState.includes('empty', 0)) {
+//       setGameWinner('Match Draw');
+//     }
+//   };
+
+//   const onPlay = (itemNumber: number) => {
+//     // if (gameWinner) {
+//     //   return Snackbar.show({
+//     //     text: gameWinner,
+//     //     backgroundColor: '#43BE31',
+//     //     textColor: '#FFFFFF',
+//     //   });
+//     // }
+
+//     if (gameState[itemNumber] === 'empty') {
+//       gameState[itemNumber] = isCircle ? 'circle' : 'cross';
+//     } else {
+//       return Snackbar.show({
+//         text: 'This place is allrady Filled',
+//         backgroundColor: '#E71C23',
+//         textColor: '#FFFFFF',
+//       });
+//     }
+
+//     winner();
+//   };
+
+//   return (
+//     <SafeAreaView>
+//       <View style={styles.container}>
+//         <View style={styles.playerNameBox}>
+//           <Text style={styles.playerName}>Player O's Turn</Text>
+//         </View>
+//         <FlatList
+//           numColumns={3}
+//           data={gameState}
+//           style={styles.playground}
+//           renderItem={({item, index}) => (
+//             <Pressable
+//               style={styles.box}
+//               key={index}
+//               onPress={() => onPlay(index)}>
+//               <Text style={styles.playText}>
+//                 <Icons name={item} />
+//               </Text>
+//             </Pressable>
+//           )}
+//         />
+
+//         <View>
+//           <TouchableOpacity style={styles.resetBtn} onPress={resetGame}>
+//             <Text style={styles.resetBtnText}>Reload Game</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     justifyContent: 'center',
+//     alignItems: 'stretch',
+//     marginHorizontal: 10,
+//     marginVertical: 20,
+//     width: undefined,
+//     height: undefined,
+//   },
+//   playerNameBox: {
+//     backgroundColor: '#0A79DF',
+//     alignItems: 'center',
+//     padding: 10,
+//     borderRadius: 8,
+//     borderWidth: 0.5,
+//     borderColor: '#FFFFFF',
+//   },
+//   playerName: {
+//     color: '#FFFFFF',
+//     fontSize: 18,
+//     fontWeight: '500',
+//   },
+//   playground: {
+//     // display: 'flex',
+//     // justifyContent: 'center',
+//     // alignItems: 'stretch',
+//     // flexDirection: 'row',
+//     // flexWrap: 'wrap',
+//     marginVertical: 20,
+//   },
+//   box: {
+//     height: 103,
+//     width: 103,
+//     backgroundColor: '#EAF0F1',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     borderRadius: 5,
+//     margin: 5,
+//   },
+//   playText: {
+//     color: '#2B2B52',
+//   },
+//   resetBtn: {
+//     backgroundColor: '#E44236',
+//     alignItems: 'center',
+//     padding: 10,
+//     borderRadius: 8,
+//     borderWidth: 0.5,
+//     borderColor: '#FFFFFF',
+//   },
+//   resetBtnText: {
+//     color: '#FFFFFF',
+//     fontSize: 18,
+//     fontWeight: '500',
+//   },
+// });
+
+// export default App;
--- src/Components/Icons.tsx
+++ src/Components/Icons.tsx
@@ -7,15 +7,16 @@ type IconsProps = PropsWithChildren<{
   name: string;
 }>;
 
-export default function Icons({name}: IconsProps):JSX.Element {
+export default function Icons({name}: IconsProps): JSX.Element {
   switch (name) {
     case 'circle':
-      return <Icon name="circle" size={55} color="#0A79DF" />;
+      return <Icon name="circle-thin" size={55} color="#F7CD2E" />;
 
     case 'cross':
-      return <Icon name="xmark" size={55} color="#E8290B" />;
-      default :
-      return <Icon name="hand-pointer" size={55} color="#2C3335" />;
+      return <Icon name="times" size={55}  color="#38CC77" />;
+
+    default:
+      return <Icon name="pencil" size={15} color="#2C3335" />;
   }
 }
 
